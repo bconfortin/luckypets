3ee8429bb275b4880012a9fd9657e6bd11362383
Bruno Confortin <brunoconfortin@gmail.com>
2017-10-02 14:52:27 -0300

    Página pra setar as variáveis na sessão

---
+++ login-backend.php
@@ -0,0 +1,31 @@
+<?php
+    if (session_status() == PHP_SESSION_NONE) {
+        session_start();
+    }
+
+    $_SESSION['administrador'] = "";
+    $_SESSION['authToken'] = "";
+    $_SESSION['celular'] = "";
+    $_SESSION['email'] = "";
+    $_SESSION['facebook'] = "";
+    $_SESSION['id'] = "";
+    $_SESSION['imagem'] = "";
+    $_SESSION['nome'] = "";
+    $_SESSION['senha'] = "";
+    $_SESSION['telefone'] = "";
+
+    if (isset($_POST['email'])) {
+        $_SESSION['administrador'] = $_POST['administrador'];
+        $_SESSION['authToken'] = $_POST['authToken'];
+        $_SESSION['celular'] = $_POST['celular'];
+        $_SESSION['email'] = $_POST['email'];
+        $_SESSION['facebook'] = $_POST['facebook'];
+        $_SESSION['id'] = $_POST['id'];
+        $_SESSION['imagem'] = $_POST['imagem'];
+        $_SESSION['nome'] = $_POST['nome'];
+        $_SESSION['senha'] = $_POST['senha'];
+        $_SESSION['telefone'] = $_POST['telefone'];
+    }
+
+    echo '<pre>' . print_r($_SESSION, TRUE) . "Session ID: " . session_id() .'</pre>';
+?>
