344d88c27bdb237ecdd06c30feccb8eba4b3ef13
Bruno Confortin <brunoconfortin@gmail.com>
2017-10-10 14:27:15 -0300

    Layout alterado.

--- listar-usuarios.php
+++ listar-usuarios.php
@@ -29,8 +29,8 @@
 				</div>
 			</div>
 		</div>
-		<div class="container-fluid bg-f5f5f5 padver-50">
-			<div class="container">
+		<div class="container-fluid bg-f5f5f5 padver-15">
+			<div class="">
 				<div class="row">
 					<div class="col-xs-12"><!--  col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3 -->
 						<div class="padding-30 bg-fff">
