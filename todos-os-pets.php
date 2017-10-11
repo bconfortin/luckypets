6d162376ff855c83cc43e27dd678f1b143e0faa8
Bruno Confortin <brunoconfortin@gmail.com>
2017-10-11 15:41:35 -0300

    Adicionado o link interno.

--- todos-os-pets.php
+++ todos-os-pets.php
@@ -183,6 +183,7 @@
 					$("div[aria-expanded='true']").attr("aria-expanded", "false");
 				}
 
+				// filtro: tipo, porte, sexo, idade, castrado
 				$.ajax({
 				    type: 'GET',
 				    crossOrigin: true,
@@ -192,13 +193,13 @@
 					    var html = '';
 					    for (i = 0; i < x.length; i++) {
 							html += '<div class="col-xs-12 col-lg-3 col-md-4 col-sm-6 col-xs-12 mbottom-30">';
-							html += '	<a href="anuncio-animal.php" class="block card-home bg-fff unstyled-link relative">';
+							html += '	<a href="anuncio-animal.php?animalId=' + x[i].id + '" class="block card-home bg-fff unstyled-link relative">';
 							html += '       <div class="img-todos-animais" style="background-image: url(\'http://31.220.53.123:8080/luckypets-servidor/api/file/doacao/' + x[i].id + '/' + x[i].animal.imagens[0] + '\');">';
 							html += '       </div>';
 							//html += '		<img src="http://31.220.53.123:8080/luckypets-servidor/api/file/doacao/' + x[i].id + '/' + x[i].animal.imagens[0] + '" alt="' + x[i].animal.nome + '" class="img-responsive center-block" style="max-height: 206px;">';
 							html += '		<div class="absolute-pet-details">';
 							html += '			<p class="pull-left mbottom-0">';
-							html += '				Nome: ' + x[i].animal.nome + '<br>Raça: ' + x[i].animal.nome;
+							html += '				Nome: ' + x[i].animal.nome + '<br>Raça: ' + x[i].animal.raca;
 							html += '			</p>';
 							html += '			<p class="pull-right text-right mbottom-0">';
 							html += '				' + x[i].cidade + '<br>' + x[i].estado;
