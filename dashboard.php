e88cfda477574cecd63042da5ebbd2f3e9ea9338
Bruno Confortin <brunoconfortin@gmail.com>
2017-10-30 21:53:37 -0200

    Começo do Dashboard.

---
+++ dashboard.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+		<title>Lucky Pets</title>
+		<meta name="description" content=""/>
+		<?php include "head.php"; ?>
+	</head>
+	<body>
+		<?php include "header.php"; ?>
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-xs-12 padhor-0">
+					<img src="img/cat-dog-1920x450.jpg" alt="Imagem promocional" class="img-responsive hidden-xs hidden-sm">
+					<img src="img/cat-dog-993x600.jpg" alt="Imagem promocional" class="img-responsive hidden-md hidden-lg">
+					<div class="force-vertical-align black-bg-filter width-100per">
+						<div class="force-vertical-align">
+							<h1 class="text-center font-1-8em text-uppercase font-700 color-fff mtop-0 mbottom-5 text-shadow font-1-3em-xs mbottom-30-xs">Doação de cachorros e gatos</h1>
+							<p class="mbottom-30 text-center font-300 font-1-5em color-fff text-shadow hidden-xs">Veja todos os animais disponíveis para doação.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="container-fluid">
+			<div class="">
+				<div class="row">
+					<div class="col-xs-12 col-md-3 col-sm-3 col-lg-2">
+						<div class="padver-50 padver-15-xs bg-fff">
+							<h1>Dashboard</h1>
+							<ul>
+								<li><a href="">Teste</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-xs-12 col-md-9 col-sm-9 col-lg-10 bg-f5f5f5">
+						<div class="row padver-50 padver-15-xs" id="containerAnimais">
+							<div class="col-xs-12">
+								<div class="bg-fff padding-30">
+									<div class="row padver-50">
+										<div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3">
+											<h2 class="text-center font-2em text-uppercase font-700 mtop-0">Dashboard!</h2>
+											<p class="font-1-2em text-center font-300 mbottom-0">Parece que a sua busca não retornou nenhum resultado.<br>Mude os filtros ou <a href="animais-para-doacao.php" class="font-700">clique aqui para ver todos os animais cadastrados</a>.</p>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="">
+
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- <div class="container-fluid padver-50">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2">
+						<h2 class="titulo">Gostaria de doar um animal?</h2>
+						<p class="line-height-1-5 text-justify font-1-1em">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam voluptatem saepe perspiciatis ut tempore, aliquam vero non. Hic quia voluptatem laborum eum quam maiores atque non dicta! Iure, culpa, sed.</p>
+						<p class="text-center mbottom-0 mtop-30">
+							<a href="http://www.loumarturismo.com.br/" class="btn btn-gradient force-radius-0 text-uppercase padhor-30" target="_blank">Saiba mais</a>
+						</p>
+					</div>
+				</div>
+			</div>
+		</div> -->
+		<?php include "footer.php"; ?>
+		<?php include "foot.php"; ?>
+	</body>
+</html>
