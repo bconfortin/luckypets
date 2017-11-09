3a8d3a41879abcb55cbaf3ce11a90dfa106d97e8
Bruno Confortin <brunoconfortin@gmail.com>
2017-11-09 08:45:07 -0200

    Mudanças gerais.

--- head.php
+++ head.php
@@ -52,41 +52,41 @@
     }
 
     window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '347116579081153',
-      cookie     : true,  // enable cookies to allow the server to access
-                          // the session
-      xfbml      : true,  // parse social plugins on this page
-      version    : 'v2.8' // use graph api version 2.8
-    });
+        FB.init({
+          appId      : '347116579081153',
+          cookie     : true,  // enable cookies to allow the server to access
+                              // the session
+          xfbml      : true,  // parse social plugins on this page
+          version    : 'v2.8' // use graph api version 2.8
+        });
 
-    // Now that we've initialized the JavaScript SDK, we call
-    // FB.getLoginStatus().  This function gets the state of the
-    // person visiting this page and can return one of three states to
-    // the callback you provide.  They can be:
-    //
-    // 1. Logged into your app ('connected')
-    // 2. Logged into Facebook, but not your app ('not_authorized')
-    // 3. Not logged into Facebook and can't tell if they are logged into
-    //    your app or not.
-    //
-    // These three cases are handled in the callback function.
+        // Now that we've initialized the JavaScript SDK, we call
+        // FB.getLoginStatus().  This function gets the state of the
+        // person visiting this page and can return one of three states to
+        // the callback you provide.  They can be:
+        //
+        // 1. Logged into your app ('connected')
+        // 2. Logged into Facebook, but not your app ('not_authorized')
+        // 3. Not logged into Facebook and can't tell if they are logged into
+        //    your app or not.
+        //
+        // These three cases are handled in the callback function.
 
-    FB.login(function(loginResponse) {
-      FB.api('/me', {fields: 'name, email, picture.type(large)'}, function(response) {
-      console.log(response.id);
-    });
-    }, {scope: 'email, public_profile'});
+        FB.login(function(loginResponse) {
+            FB.api('/me', {fields: 'name, email, picture.type(large)'}, function(response) {
+            console.log(response.id);
+        });
+        }, {scope: 'email, public_profile'});
 
     };
 
     // Load the SDK asynchronously
     (function(d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s); js.id = id;
-      js.src = "//connect.facebook.net/en_US/sdk.js";
-      fjs.parentNode.insertBefore(js, fjs);
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));
 
     </script>
